3cee03c5d1f70ff4fb9a841ef4998f7017c3183b
Bernardo Alves <51270900+Byteend@users.noreply.github.com>
2022-12-17 19:05:24 -0300

    Alterações refeitas
    Refiz as alterações que foram desfeitas por motivo de incompatibilidade de versão da unity. Todas as alterações (Pulo do jogador, obstaculo e gerador do obstaculo) foram refeitas.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private bool onAir;
+
+    Rigidbody2D Rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+        jumpSpeed = 900f;
+        onAir = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        JumpPlayer();
+    }
+
+    void JumpPlayer()
+    {
+        if(Input.GetKey(KeyCode.Space) && !onAir )
+        {
+            Rigidbody.AddForce(Vector3.up * jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            onAir = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Plataforma")
+        {
+            onAir = false;
+            Debug.Log("No Chão");
+        }
+        
+    }
+}
