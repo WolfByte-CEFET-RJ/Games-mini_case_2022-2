c944a02d8acab87e935d7fc96a34cf55ee0e8371
Bernardo Alves <51270900+Byteend@users.noreply.github.com>
2023-01-12 21:54:41 -0300

    Sistema de pontuação e vidas (Sem texto/interface)
    Adicionei o sistema de pontuação e o sistema de vidas no jogo.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,20 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpSpeed;
     [SerializeField] private bool onAir;
     [SerializeField] private Transform gameOver;
+    [SerializeField] public float pontuacao;
+    [SerializeField] private bool gameOn;
+    [SerializeField] private float pointsPerSeconds;
+    [SerializeField] private int vidas;
+    private int maxVidas = 3;   
 
     Rigidbody2D Rigidbody;
     
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        gameOn = true;
+        vidas = maxVidas;
+        pointsPerSeconds = 13;
         Rigidbody = GetComponent<Rigidbody2D>();
         jumpSpeed = 900f;
         onAir = false;
@@ -22,6 +30,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(gameOn)
+        {
+            pontuacao += pointsPerSeconds * Time.deltaTime;
+        }
+
+        if(vidas <= 0)
+        {
+            fimDeJogo();
+        }
         
     }
 
@@ -45,24 +62,31 @@ public class Player : MonoBehaviour
         {
             onAir = false;
             Debug.Log("No Chão");
+           
+           
         }
 
         if(collision.gameObject.CompareTag("obstaculo"))
         {
-            gameOver.gameObject.SetActive(true);
-            Time.timeScale = 0;  
-        }else
-        {
-            gameOver.gameObject.SetActive(false);
-            Time.timeScale = 1;
+            vidas--;
+            Destroy(collision.gameObject);
+            
         }
         
     }
-
+    private void fimDeJogo()
+    {
+        gameOver.gameObject.SetActive(true);
+        Time.timeScale = 0;
+        gameOn = false;  
+    }
     void RetryGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+        vidas = maxVidas;
+        pontuacao = 0;
+        gameOn = true;
     }
 
 }
--- Assets/Scripts/obstaculoTerrestre.cs
+++ Assets/Scripts/obstaculoTerrestre.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class obstaculoTerrestre : MonoBehaviour
 {
     private float speed;
+    public GameObject player;
+    private Player jogador;
+    private bool pontoColetado;
     // Start is called before the first frame update
     void Start()
     {
+        pontoColetado = true;
+        jogador = GameObject.Find("Player").GetComponent<Player>();
         speed = 7f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if(jogador.transform.position.x > transform.position.x && pontoColetado)
+         {
+            pontoColetado = false;
+            jogador.pontuacao += 50;
+         }
+         if(transform.position.x <= jogador.transform.position.x - 5 )
+         {
+            Destroy(this.gameObject);
+            
+         }
     }
+    
 
     void FixedUpdate()
     {
+        
+
         this.gameObject.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
     }
 }
